97883ab9702776ed397ac9c968e5908823dc2d16
Steven Ruiz <ruiz.steven.j@gmail.com>
2019-04-06 18:11:15 -0700

    simple calculator refactored with rubocop check

--- simple_calc_refactored.rb
+++ simple_calc_refactored.rb
@@ -19,15 +19,15 @@ def operation_to_message(op)
   end
 end
 
-prompt("Welcome to Calculator!")
+prompt('Welcome to Calculator!')
 
 name = ''
 loop do
-  prompt("Please enter your name:")
+  prompt('Please enter your name:')
   name = gets.chomp.capitalize
 
-  if name.empty?
-    prompt("Make sure to use a valid name.")
+  if name == ''
+    prompt('Make sure to use a valid name.')
   else
     break
   end
@@ -38,25 +38,25 @@ prompt("Hello #{name}")
 loop do
   number1 = ''
   loop do
-    prompt("What is the first number?")
+    prompt('What is the first number?')
     number1 = gets.chomp.to_i
 
     if valid_number?(number1)
       break
     else
-      prompt("There seems to be an invalid input. Please try again.")
+      prompt('There seems to be an invalid input. Please try again.')
     end
   end
 
   number2 = ''
   loop do
-    prompt("What is the second number?")
+    prompt('What is the second number?')
     number2 = gets.chomp.to_i
 
     if valid_number?(number2)
       break
     else
-      prompt("There seems to be an invalid input. Please try again.")
+      prompt('There seems to be an invalid input. Please try again.')
     end
   end
 
@@ -74,29 +74,29 @@ loop do
   loop do
     operator = gets.chomp
 
-    if %w(a s m d).include?(operator)
+    if %w[a s m d].include?(operator)
       break
     else
-      prompt("Please choose a valid operation a, s, m, or d")
+      prompt('Please choose a valid operation a, s, m, or d')
     end
   end
 
   prompt("#{operation_to_message(operator)} the two numbers is...")
 
   result = case operator
-          when 'a'
-            number1 + number2
-          when 's'
-            number1 - number2
-          when 'm'
-            number1 * number2
-          when 'd'
-            number1.to_f / number2
-  end
+           when 'a'
+             number1 + number2
+           when 's'
+             number1 - number2
+           when 'm'
+             number1 * number2
+           when 'd'
+             number1.to_f / number2
+           end
 
   prompt("The result is #{result}")
 
-  prompt("Do you want to perfor another calculation? (Y to calculate again)")
+  prompt('Do you want to perfor another calculation? (Y to calculate again)')
   answer = gets.chomp.downcase
   break unless answer == 'y'
 end
